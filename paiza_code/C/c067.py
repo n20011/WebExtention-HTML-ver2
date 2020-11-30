c431f001d7a59200a1d03d2240ce2791afc45308
n20011 <n20011@std.it-college.ac.jp>
2020-11-30 13:06:19 +0900

    11/30/09:33:41

--- paiza_code/C/c067.py
+++
@@ -1,9 +0,0 @@
-N, nunber = map(int, input().split())
-Base_N = bin(nunber)
-
-#print(N, Base_N)
-
-for _ in range(N):
-    place_N = int(input())
-    #print(place_N)
-    print(Base_N[place_N * -1])
